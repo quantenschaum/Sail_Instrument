edb23056447884ac36263c838c8a4c1f1e6bc018
quantenschaum <woxpox@posteo.de>
2026-04-28 15:50:45 +0200

    leave VPOL unset, use avg STW

--- plugin.py
+++ plugin.py
@@ -629,9 +629,9 @@ class Plugin(object):
             assert 0 <= gybe_angle < 180
 
             data.VMCA, data.VMCB = -1, -1
-            data.VPOL, data.POLAR = 0, 0
+            # data.VPOL, data.POLAR = 0, 0
 
-            data.LLSV = data.LLPV = data.STW # velocities on laylines
+            data.LLSV = data.LLPV = data.STWF # velocities on laylines
 
             fixed_laylines=False
             if upwind and tack_angle or not upwind and gybe_angle:
